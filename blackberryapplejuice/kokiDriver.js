1f0247450b2694966d88531d6d901cc19b1fc917
Sunil Shah <ss@ss21k.co.uk>
2013-04-20 16:27:43 +0100

    Fixed NAN.

--- blackberryapplejuice/kokiDriver.js
+++ blackberryapplejuice/kokiDriver.js
@@ -1,7 +1,7 @@
 var last_seen_marker = 0;
 var max_move = 1 //Arbitrary
 var steer = 0;
-var move = 0.5;
+var move = 0.3;
 var accelerate = 1.05;
 var clockwise = true;
 
@@ -11,7 +11,7 @@ var previous_bearing = 0;
 
 var driver = {}
 
-driver.drive = function (steer, move) {	
+driver.drive = function (client, steer, move) {	
 	client.steer(steer);
 	client.move(move);
 	console.log('Steer:' + steer.toString() + ' Move: ' + move.toString());
@@ -21,7 +21,7 @@ driver.scan_for_markers = function (client) {
 	//Go the other way!
 	steer = last_steer * -1; 
 	driver.accelerate(); //Ambitious
-	driver.drive(steer, move);
+	driver.drive(client, steer, move);
 	last_steer = steer;
 }
 
@@ -38,33 +38,33 @@ driver.approach_marker = function (client, marker)  {
 				if (clockwise) {
 					//Marker is on the outside edge
 					var marker_location = marker.centre.world;
-					steer = marker_location.x * -3;
+					steer = 0.5;
 					driver.accelerate();
-					driver.drive(steer, move);
+					driver.drive(client, steer, move);
 				}
 			} else {
 				if (clockwise) {
 					//Marker is on the inside edge
 					var marker_location = marker.centre.world;
-					steer = marker_location.x * -3;
+					steer = -0.5;
 					driver.accelerate();
-					driver.drive(steer, move);
+					driver.drive(client, steer, move);
 				}
 			}    	
 		} else if (marker.code < last_seen_marker) {
 			driver.decelerate();
 			driver.scan_for_markers(client); //We shouldn't be going backwards!
 		} else {
-			steer = Math.cos(marker.center.world.y * 20);
-            console.log('maintain going toward marker at steering - ' + steer);
-            client.steer(steer);
-            client.move(move);
+			//steer = Math.cos(marker.center.world.y * 20);
+            //console.log('maintain going toward marker at steering - ' + steer);
+            driver.drive(client, steer, move);
 		}
         last_seen_marker = marker.code;
 	}
 }
 
 driver.accelerate = function () {
+	console.log('Accelerate');
 	move *= accelerate;
 
 	if (move > max_move) {
@@ -73,6 +73,7 @@ driver.accelerate = function () {
 }
 
 driver.decelerate = function () {
+	console.log('Decelerate');
 	move /= accelerate;
 }
 
@@ -86,7 +87,7 @@ driver.avoid_walls = function(client, marker) {
             steer = -last_steer
             previous_bearing = Math.abs(marker.bearing.x);
         }
-        driver.drive(steer, move);
+        driver.drive(client, steer, move);
         return true;
     }
     return false;
@@ -94,11 +95,12 @@ driver.avoid_walls = function(client, marker) {
 
 
 driver.distance_to_marker = function (marker) {
+	console.log(marker);
 	return marker.centre.world.y;
 }
 
 driver.distance_to_wall = function (marker) { 
-    angle = marker.rotation.x * (180 / Math.pi);
+    angle = marker.rotation.x * (180 / Math.PI);
     console.log('Angle of us to marker ' + angle);
     return marker.centre.world.y * Math.cos(angle);
 }
