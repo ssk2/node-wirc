72af339635f48ece58e7cd6c1169fd86be34c969
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-04-20 12:48:31 +0100

    Added info on marker info to info/marker-data.js

--- info/marker-data.js
+++ info/marker-data.js
@@ -8,3 +8,11 @@ var markers = [ { code: 0,
     rotation: { x: -1.6446934938430786, y: -6.735818386077881 },
     bearing: { x: -2.375349521636963, y: 0 },
     vertices: [ [Object], [Object], [Object], [Object] ] } ];
+
+
+
+
+centre.world...
+    x = left+right          // + to the right of the car
+                            // - to the left of the car
+    y = distance from car   // value = meters from camera
