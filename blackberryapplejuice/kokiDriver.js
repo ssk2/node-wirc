0f15f5321e9f19b6327ad0fe2af36e15b124d00b
Sunil Shah <ss@ss21k.co.uk>
2013-04-20 13:34:30 +0100

    Driver logic.

--- blackberryapplejuice/kokiDriver.js
+++ blackberryapplejuice/kokiDriver.js
@@ -2,26 +2,50 @@ var last_seen_marker = 0;
 var max_speed = 1 //Arbitrary
 var steer = 0;
 var move = 0;
+var clockwise = true;
 //What is the expected marker orientation? Clockwise or anticlockwise? Initialise appropriately.
 
+var last_steer = 0;
+var last_move = 0;
+
 driver.scan = function (client) {
-	
-	//Reverse rotation and speed
-	    move = move * 0.8;
-    client.move(move);
+	//Go the other way!
+	steer = last_steer * -1; 
+	move = last_move * -1;
+	client.steer(steer);
+	client.move(move);
+	last_steer = steer;
+	last_move = move;
 }
 
 driver.drive = function (client, marker)  {
-    //Classify marker (left or right?)
     //Is marker the next marker we're expecting? (i.e. < last marker)
-
     //Drive towards it (clever driving logic here that slows down when we hit a marker)
-
-	var marker_location = marker.centre.world;
-	steer = marker_location.x * -3;
-	move = (marker_location.y - 1) * 0.7;
-	client.steer(steer);
-	client.move(move);
+    if (marker.code > last_seen_marker)
+    {
+    	last_seen_marker = marker.code;
+	    if (marker.code % 2 == 0) {
+	    	if (clockwise) {
+	    		//Marker is on the outside edge
+				var marker_location = marker.centre.world;
+				steer = marker_location.x * -3;
+				move = (marker_location.y - 1) * 0.7;
+				client.steer(steer);
+				client.move(move);
+	    	}
+	    } else {
+	    	if (clockwise) {
+	    		//Marker is on the inside edge
+				var marker_location = marker.centre.world;
+				steer = marker_location.x * -3;
+				move = (marker_location.y - 1) * 0.7;
+				client.steer(steer);
+				client.move(move);
+	    	}
+	    }    	
+    } else {
+    	//Scan or something. We shouldn't be going towards lower ranked markers.
+    }
 }
 
 module.exports = driver;
