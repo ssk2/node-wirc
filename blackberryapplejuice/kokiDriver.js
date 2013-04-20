cb11c271eee61799d71728bcc2fed6f9f72ccd75
Sunil Shah <ss@ss21k.co.uk>
2013-04-20 13:24:00 +0100

    More refactoring.

--- blackberryapplejuice/kokiDriver.js
+++ blackberryapplejuice/kokiDriver.js
@@ -1,27 +1,25 @@
-val last_seen_marker = 0;
+var last_seen_marker = 0;
+var max_speed = 1 //Arbitrary
+var steer = 0;
+var move = 0;
+//What is the expected marker orientation? Clockwise or anticlockwise? Initialise appropriately.
 
-
-driver.scan = function (client, marker) {
-	//Can't see marker?
-		//Look for marker
-	//Move left
-	//Move right
-
-    move = move * 0.8;
+driver.scan = function (client) {
+	
+	//Reverse rotation and speed
+	    move = move * 0.8;
     client.move(move);
-
 }
 
-driver.drive = function (client, markers)  {
-	//Drive towards marker?
+driver.drive = function (client, marker)  {
     //Classify marker (left or right?)
     //Is marker the next marker we're expecting? (i.e. < last marker)
 
     //Drive towards it (clever driving logic here that slows down when we hit a marker)
 
-	var marker = markers[0].centre.world;
-	steer = marker.x * -3;
-	move = (marker.y - 1) * 0.7;
+	var marker_location = marker.centre.world;
+	steer = marker_location.x * -3;
+	move = (marker_location.y - 1) * 0.7;
 	client.steer(steer);
 	client.move(move);
 }
--- blackberryapplejuice/kokiRacer.js
+++ blackberryapplejuice/kokiRacer.js
@@ -1,8 +1,9 @@
 var client = require('../lib/car');
 var childProcess = require('child_process');
+var driver = require("./kokiDriver");
 var koki = require("../lib/koki");
 var wrapper = require("./kokiWrapper");
-var driver = require("./kokiDriver");
+
 var serialNumber = '0000981';
 
 client.discover()
@@ -10,13 +11,10 @@ client.discover()
     .then(function() { return client.enable(); })
     .then(function() {
 
-        //What is the expected marker orientation? Clockwise or anticlockwise? Initialise appropriately.
-
         var device = client.chosenDevice();
-        var steer = 0;
-        var move = 0;
 
         client.startCamera(0, function(data) {
+
             koki.findMarkers(data.image, function(err, markers) {
                 if (markers[0]) {     
                     //Found marker
@@ -25,7 +23,7 @@ client.discover()
                 }
                 else {
                     //Look for marker
-                    driver.scan(client);
+                   driver.scan(client);
                 }
             });
         });
