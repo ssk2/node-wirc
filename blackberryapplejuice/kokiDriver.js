0993dae8dd141fe9bf382fc2b126a6e0991ec9e0
Sunil Shah <ss@ss21k.co.uk>
2013-04-20 15:06:15 +0100

    Integrated walls method.

--- blackberryapplejuice/kokiDriver.js
+++ blackberryapplejuice/kokiDriver.js
@@ -11,17 +11,13 @@ var last_move = 0.5;
 var driver = {}
 
 driver.drive = function (client, data) {	
-	//Keep going straight
-
-	//Approach Marker
-
 	if (data.getLastMarkers().length > 0) {
 		console.log('Seen a marker');
 		//Seen a new marker
         marker = data.getFurthestMarker();
-        driver.approach(client, marker);
+        driver.approach_marker(client, marker);
 	} else {
-		driver.scan(client);
+		driver.scan_for_markers(client);
 	}
 }
 
@@ -37,54 +33,54 @@ driver.scan_for_markers = function (client) {
 }
 
 driver.approach_marker = function (client, marker)  {
-	// Have 
-
     //Is marker the next marker we're expecting? (i.e. < last marker)
     //Drive towards it (clever driving logic here that slows down when we hit a marker)
-    console.log('Moving towards marker');
 
-    if (marker.code > last_seen_marker)
-    {
-    	last_seen_marker = marker.code;
-	    if (marker.code % 2 == 0) {
-	    	console.log('Even marker');
-	    	if (clockwise) {
-	    		//Marker is on the outside edge
-				var marker_location = marker.centre.world;
-				steer = marker_location.x * -3;
-				move = (marker_location.y - 1) * 0.7;
-				client.steer(steer);
-				client.move(move);
-	    	}
-	    } else {
-	    	console.log('Odd Marker');
-	    	if (clockwise) {
-	    		//Marker is on the inside edge
-				var marker_location = marker.centre.world;
-				steer = marker_location.x * -3;
-				move = (marker_location.y - 1) * 0.7;
-				client.steer(steer);
-				client.move(move);
-	    	}
-	    }    	
-    } else {
-    	//Scan or something. We shouldn't be going towards lower ranked markers.
-    }
+    if (!driver.avoid_walls(client, marker)) {
+		console.log('Moving towards marker');
+
+		if (marker.code > last_seen_marker) {
+			last_seen_marker = marker.code;
+			if (marker.code % 2 == 0) {
+				console.log('Even marker');
+				if (clockwise) {
+					//Marker is on the outside edge
+					var marker_location = marker.centre.world;
+					steer = marker_location.x * -3;
+					move = (marker_location.y - 1) * 0.7;
+					client.steer(steer);
+					client.move(move);
+				}
+			} else {
+				console.log('Odd Marker');
+				if (clockwise) {
+					//Marker is on the inside edge
+					var marker_location = marker.centre.world;
+					steer = marker_location.x * -3;
+					move = (marker_location.y - 1) * 0.7;
+					client.steer(steer);
+					client.move(move);
+				}
+			}    	
+		} else {
+		//Scan or something. We shouldn't be going towards lower ranked markers.
+		}
+	}
 }
 
-driver.avoidWalls = function(client, marker) {
+driver.avoid_walls = function(client, marker) {
     var distance_to_wall = driver.distance_to_wall(marker);
-    
-    //if we're too close to a wall then get away before doing anything else
+
     if (distance_to_wall <= 0.15) {
+    	console.log('Avoiding wall!');
         if (Math.abs(marker.bearing.x) < previous_bearing) {
             steer = -last_steer
             previous_bearing = Math.abs(marker.bearing.x);
         }
-        client.steer(move);
+
+        client.steer(steer);
         client.move(move);
-        
-        //return a value to show that we're performing evasive actions
+        console.log('Steer:' + steer.toString() + ' Move: ' + move.toString());
         return true;
     }
     return false;
