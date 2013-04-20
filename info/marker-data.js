1b11aa612def81fdb26938284e019dcd30049fd5
Toby Griffiths <toby@cubicmushroom.co.uk>
2013-04-20 12:18:26 +0100

    Added marker data

---
+++ info/marker-data.js
@@ -0,0 +1,10 @@
+var markers = [ { code: 0,
+    centre: { image: [Object], world: [Object] },
+    rotation: { x: 20.37822151184082, y: 4.3019185066223145 },
+    bearing: { x: -1.5608984231948853, y: 0 },
+    vertices: [ [Object], [Object], [Object], [Object] ] },
+  { code: 1,
+    centre: { image: [Object], world: [Object] },
+    rotation: { x: -1.6446934938430786, y: -6.735818386077881 },
+    bearing: { x: -2.375349521636963, y: 0 },
+    vertices: [ [Object], [Object], [Object], [Object] ] } ];
